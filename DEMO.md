922590c404d9688883e2f70a986338184cdae272
Casey Wylie <cmwylie19@gmail.com>
2021-02-12 13:57:54 -0500

    clean branch

--- DEMO.md
+++
@@ -1,5 +0,0 @@
-Envoy 101	 
-Debug demo 
-Rate limiting
-WAF
-Gloo running on API gateway mode doing A/B deployments. 
--- src/models/tasks.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const Task = new Schema({
-  title: { type: String },
-  ts: { type: String },
-});
-
-module.exports = mongoose.model("Task", Task);
