0e96007a9cc608dadf1fa77d4742b02e8ee57a4c
Casey Wylie <cmwylie19@gmail.com>
2021-02-12 13:44:41 -0500

    update

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -10,13 +10,22 @@ router.use(function (req, res, next) {
   next();
 });
 
-<<<<<<< HEAD
+const express = require("express");
+const router = express.Router();
+const task = require("../controllers/tasks");
+
+router.use(function (req, res, next) {
+  console.log("/" + req.method);
+  console.log("x-forwarded-for: ", req.headers["x-forwarded-for"]);
+  console.log("Remote address: ", req.connection.remoteAddress);
+  console.log("Request Path: ", req.path);
+  next();
+});
+
 router.get("/", function (req, res) {
   res.send("Hello");
 });
 
-=======
->>>>>>> 06d1f4c68b1a53cb974a685b5e4583a037d72be4
 router.post("/", function (req, res) {
   task.create(req, res);
 });
@@ -25,15 +34,16 @@ router.put("/", function (req, res) {
   task.update(req, res);
 });
 
-router.get("/", function (req, res) {
+router.get("/gettask", function (req, res) {
   task.list(req, res);
 });
 
+router.delete("/deletetask", (req, res) => {
+  task.delete(req, res);
+});
 router.get("/:id", function (req, res) {
   task.listOne(req, res);
 });
-
-router.delete("/", (req, res) => {
-  task.delete(req, res);
-});
 module.exports = router;
+
+
